bcd1112bb058f916b5653e3ca85faf0cc457297d
thousandlives <rpgreene@gmail.com>
2020-07-06 04:00:10 +0100

    Update atlas_functions.js

--- atlas_functions.js
+++ atlas_functions.js
@@ -1,3 +1,8 @@
-function testFunction() {
-	alert("Hi There!");
+var Question_Binary = {
+	verb: "dither"
+}
+
+function checkDate(dateString) {
+	date = new Date(dateString);
+	console.log(date.toString());
 }
